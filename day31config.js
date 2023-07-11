94d7804bcb3c6dee49ef758c2cf94e906d27f7a0
nasimashapla <134200983+nasimashapla@users.noreply.github.com>
2023-07-11 17:16:52 +0600

    Create day31config.js

---
+++ day31config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCJPEJLV_SSWrwASYt9p29iBy_Ws2prPo",
+    authDomain: "loginwith-e0f01.firebaseapp.com",
+    projectId: "loginwith-e0f01",
+    storageBucket: "loginwith-e0f01.appspot.com",
+    messagingSenderId: "1052153780381",
+    appId: "1:1052153780381:web:996b2d6cb5e3ac2b6ddc74"
+  };
