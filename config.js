4aab0e6f8a6c501597ad2666e1b5f837f4432e68
nasimashapla <134200983+nasimashapla@users.noreply.github.com>
2023-07-12 01:11:52 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDe_GsdsFg9DSzd6fAftGUpqI7_g0GSzHM",
+authDomain: "signinwith-31900.firebaseapp.com",
+projectId: "signinwith-31900",
+storageBucket: "signinwith-31900.appspot.com",
+messagingSenderId: "418810305196",
+appId: "1:418810305196:web:1d8a82ec0df149ddea5d09"
+  };
+  
+//   // Initialize Firebase
+
+  var app = firebase.initializeApp(firebaseConfig);
+
